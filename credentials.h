f230c4d99f9f67bae8fae68e7b669f7dab162351
Marcin W <wiewmarcin@gmail.com>
2022-03-23 23:00:48 +0100

    Extend sensitive information

--- credentials.h
+++ credentials.h
@@ -1,3 +1,5 @@
-#define mySSID "######" //router's SSID
-#define myPASSWORD "######" //password
-#define myOTAPASS "######" //OTApass to updates
+#define mySSID "######" //Add your router's SSID
+#define myPASSWORD "######" //Add the password
+#define myOTAPASS "######" //Add OTA pass to updates
+#define myAPIKEY "######" //Add apikey to access secured services
+#define myJSSECRET "######" //Add jssecret to encrypt sensitive data on frontend
